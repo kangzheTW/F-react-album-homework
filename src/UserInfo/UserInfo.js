e11181239d93b0402284a7abd88bee07f8f47f92
Zhe Kang <zkang@thoughtworks.com>
2020-07-11 12:15:58 +0800

    show blue color for links

--- src/UserInfo/UserInfo.js
+++ src/UserInfo/UserInfo.js
@@ -5,7 +5,7 @@ function UserInfoRow(props) {
   return (
     <div className="user-info-row">
       <label className="info-title">{props.label}</label>
-      <label className="info-content">{props.content}</label>
+      <label className={props.isLink ? ' link' : ''}>{props.content}</label>
     </div>
   );
 }
@@ -18,10 +18,10 @@ function UserInfo(props) {
         <>
           <h2>{user.name}</h2>
           <h3>Contact Me @</h3>
-          <UserInfoRow label="Email" content={user.email} />
-          <UserInfoRow label="Phone" content={user.phone} />
-          <UserInfoRow label="Website" content={user.website} />
-          <UserInfoRow label="Company" content={user.company.name} />
+          <UserInfoRow label="Email" content={user.email} isLink />
+          <UserInfoRow label="Phone" content={user.phone} isLink />
+          <UserInfoRow label="Website" content={user.website} isLink />
+          <UserInfoRow label="Company" content={user.company.name} isLink={false} />
         </>
       )}
     </aside>
